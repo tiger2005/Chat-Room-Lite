3c2a64a734817d9c5159bce20c6a1beb76619e7e
tiger2005 <41613797+tiger2005@users.noreply.github.com>
2020-11-27 19:00:28 +0800

    Add files via upload

--- Client.js
+++ Client.js
@@ -1,35 +1,59 @@
 var ws,output=$('.ChatRoom .Output'),DOC_title=$('title');
-var MSGC=0,WFocus=true,M_Notice=false;
+var MSGC=0,WFocus=true,M_Notice=true;
 var User=new Object(),Server=new Object();
+var Commands={
+    "cls":{
+        fun:()=>{output.empty();}
+    },
+    "exit":{
+        fun:()=>{ws.close()}
+    },
+    "notice":{
+        fun:()=>{
+            M_Notice=!M_Notice;
+			document.getElementById('AlertS').className=(M_Notice?"fas fa-bell":"fas fa-bell-slash");
+            Write(`<i class="fas fa-wrench" style="width:20px"></i> Message notice = ${M_Notice}\n`,{"color":"#13c60d"})
+        }
+    }
+},S_Status=0,S_Interface=true;
 if (!window.WebSocket){
     output.html('');
     Write(`Fucking Error: No Websocket support.\n`,{'color':"#e7483f"});
 }
+function ChangeInputContent(str){
+	$('.Input').val(str);
+}
+function AddInputContent(str){
+	$('.Input').val($('.Input').val()+str);
+}
 function Initalize(){
     ws=new WebSocket(`ws://${User.host}:${User.port}`);
-    Commands.cls.fun();
     ws.onopen=()=>{
         ws.send(JSON.stringify({
             headers:{
-                "Content_Type":'application/init',
-                "Set_Name":User.name
+                "Content_Type":'application/userlist'
             }
         }));
     }
     ws.onclose=(CS_E)=>{
         console.log(CS_E);
         $('.Status .Output').html(`<span style='color:#e7483f;'>Cannot find the service</span>`);
-        Write(`[ERROR] Error Code : ${CS_E.code}\nCannot find the service.`,{'color':"#e7483f"});
+        Write(`<i class="fa fa-exclamation-circle" style="width:20px"></i> Error Code : ${CS_E.code}\nCannot find the service.`,{'color':"#e7483f"});
     }
     ws.onmessage=(msg)=>{
         // console.log(msg);
+        // ${Server.usrList.join(', ')}
         msg=JSON.parse(msg.data);
+        if(msg.headers['Content_Type']==='application/userlist'){
+            Server=msg.headers.Set_serverinfo;
+            Write(`<i class="fas fa-users" style="width:20px"></i> User(s) : ${Server.usrList.join(', ')}\n`);
+        }
         if(msg.headers['Content_Type']==='application/init'){
             Server=msg.headers.Set_serverinfo;
-            $('.Status .Output').html('Chat Room');
-            $('.UserInfo .Output').html(`Chat Name : ${Server.name}\nUser : ${User.name}\nUser List : \n${Server.usrList.map(x=>'|---'+x).join('\n')}`);
+            $('.Status .Output').html('<i class="fas fa-close" onclick="ChangeInputContent(\'/exit\')" style="color:#e7483f;width:20px"></i><i class="fas fa-bell" id="AlertS" onclick="ChangeInputContent(\'/notice\')" style="width:20px"></i> Chat Room');
+            $('.UserInfo .Output').html(`<i class="fas fa-comments" style="width:20px"></i> Chat Name : ${Server.name}\n<i class="fas fa-user" style="width:20px"></i> User : ${User.name}\n<i class="fas fa-users" style="width:20px"></i> User List : \n${Server.usrList.map(x=>'   <i class="fas fa-check" style="color:#13c60d;width:20px"></i><i class="fas fa-at" onclick="AddInputContent(\'@'+x.replace("\"","&quot;").replace("'","&apos;")+' \')" style="width:20px"></i> '+x).join('\n')}`);
             output.html('');
-            Write(`[NOTE] Chat name : ${Server.name}\nUser(s) : ${Server.usrList.join(', ')}\n               JS Chat Room\n/cls      | to clear the messages.\n/exit     | to exit the chat room.\n/notice   | notice on new message.\n`,{"color":"#13c60d"});
+            Write(`<i class="fas fa-info-circle" style="width:20px"></i> Chat name : ${Server.name}\nUser(s) : ${Server.usrList.join(', ')}\n               JS Chat Room\n/cls      | to clear the messages.\n/exit     | to exit the chat room.\n/notice   | notice on new message.\n`,{"color":"#13c60d"});
         }
         if(msg.headers['Content_Type']==='application/message'){
             if(msg.headers['Style']){
@@ -45,49 +69,42 @@ var RemoteCommands={
     "UsrAdd":(Para)=>{
         if(Server.usrList){
             Server.usrList.push(Para[0]);
-            $('.UserInfo .Output').html(`Chat Name : ${Server.name}\nUser : ${User.name}\nUser List : \n${Server.usrList.map(x=>'|---'+x).join('\n')}`);
+            $('.UserInfo .Output').html(`<i class="fas fa-comments" style="width:20px"></i> Chat Name : ${Server.name}\n<i class="fas fa-user" style="width:20px"></i> User : ${User.name}\n<i class="fas fa-users" style="width:20px"></i> User List : \n${Server.usrList.map(x=>'   <i class="fas fa-check" style="color:#13c60d;width:20px"></i><i class="fas fa-at" onclick="AddInputContent(\'@'+x.replace("\"","&quot;").replace("'","&apos;")+' \')" style="width:20px"></i> '+x).join('\n')}`);
         }
     },
     "UsrDel":(Para)=>{
         if(Server.usrList){
             Server.usrList.splice(Server.usrList.indexOf(Para[0]),1);
-            $('.UserInfo .Output').html(`Chat Name : ${Server.name}\nUser : ${User.name}\nUser List : \n${Server.usrList.map(x=>'|---'+x).join('\n')}`);
+            $('.UserInfo .Output').html(`<i class="fas fa-comments" style="width:20px"></i> Chat Name : ${Server.name}\n<i class="fas fa-user" style="width:20px"></i> User : ${User.name}\n<i class="fas fa-users" style="width:20px"></i> User List : \n${Server.usrList.map(x=>'   <i class="fas fa-check" style="color:#13c60d;width:20px"></i><i class="fas fa-at" onclick="AddInputContent(\'@'+x.replace("\"","&quot;").replace("'","&apos;")+' \')" style="width:20px"></i> '+x).join('\n')}`);
         }
     }
 }
-var Commands={
-    "cls":{
-        fun:()=>{output.empty();}
-    },
-    "exit":{
-        fun:()=>{ws.close()}
-    },
-    "notice":{
-        fun:()=>{
-            M_Notice=!M_Notice;
-            Write(`[NOTE] Message notice=${M_Notice}\n`,{"color":"#13c60d"})
-        }
-    }
-},S_Status=0,S_Interface=true;
 function Send(msg){
     S_Interface=false;
     if(S_Status!==3){
         if(S_Status===0){
             User.host=msg;
             S_Status++;
-            Write(`[NOTE] Get Host IP : ${User.host}\n`,{'color':'#13c60d'});
+            Write(`<i class="fas fa-check" style="color:#13c60d;width:20px"></i> Get Host IP : ${User.host}\n`,{'color':'#13c60d'});
             Write(`Service Port : \n`);
         }else if(S_Status===1){
             User.port=parseInt(msg);
             S_Status++;
-            Write(`[NOTE] Get Service Port : ${User.port}\n`,{'color':'#13c60d'});
-            Write(`Your Name : \n`);
+            Write(`<i class="fas fa-check" style="color:#13c60d;width:20px"></i> Get Service Port : ${User.port}\n`,{'color':'#13c60d'});
+            Initalize();
+            Write(`Your Unique Name : \n`);
         }else if(S_Status===2){
             User.name=msg;
             S_Status++;
-            Initalize();
+            Commands.cls.fun();
+            ws.send(JSON.stringify({
+                headers:{
+                    "Content_Type":'application/init',
+                    "Set_Name":User.name
+                }
+            }));
         }
-    }else if(msg[0]==='/'){
+    }else if(msg[0]==='/'&&Commands[msg.substr(1)]){
         Commands[msg.substr(1)].fun();
     }else {
         ws.send(JSON.stringify({
@@ -99,18 +116,18 @@ function Send(msg){
     }
 }
 window.onload=()=>{
-    Write(`Host IP : \n|-- Public Room: 49.234.17.22:8080 <span style='color:grey;'>·Pending</span>\n`);
+    Write(`Host IP : \n<i class="fa fa-spinner fa-spin" style="width:20px"></i> Public Room: 49.234.17.22:8080 <span style='color:grey;'>·Pending</span>\n`);
     let Ping=new WebSocket('ws://49.234.17.22:8080');
     Ping.onerror=()=>{
         if(S_Interface===true){
             output.empty();
-            Write(`Host IP : \n|-- Public Room: 49.234.17.22:8080 <span style='color:#e7483f;'>·Offline</span>\n`);
+            Write(`Host IP : \n<i class="fas fa-chain-broken" style="color:#e7483f;width:20px"></i> Public Room: 49.234.17.22:8080 <span style='color:#e7483f;'>·Offline</span>\n`);
         }
     };
     Ping.onopen=()=>{
         if(S_Interface===true){
             output.empty();
-            Write(`Host IP : \n|-- Public Room: 49.234.17.22:8080 <span style='color:#13c60d;'>·Online</span>\n`);
+            Write(`Host IP : \n<i class="fas fa-link" style="color:#13c60d;width:20px"></i> Public Room: 49.234.17.22:8080 <span style='color:#13c60d;'>·Online</span>\n`);
             Ping.close();
         }
     }
@@ -131,6 +148,35 @@ function MSGC_SS(){
 }
 function Write(msg,style){
     MSGC_SS();
+    let EXC='';
+    var nextCharacter;
+    //在实现@功能的时候，我只能通过创建一个用于Exchange的数组来完成/fn
+    for(let i=0,r;i<msg.length;i=r+1){
+        r=i;
+        let ifMatched=false;
+        if(msg[i]==='@'){
+            for(let j=0;j<Server.usrList.length;j++)
+                if(User.name === Server.usrList[j] && i+Server.usrList[j].length<msg.length){
+                    ifMatched=true;
+                    for(let k=1;k<=Server.usrList[j].length;k++)
+                        if(Server.usrList[j][k-1]!=msg[i+k]){
+                            ifMatched=false;
+                            break;
+                        }
+                    let nextCharacter = i+Server.usrList[j].length+1;
+                    if(ifMatched && (nextCharacter>=msg.length || msg[nextCharacter]===' ' || msg[nextCharacter]==='\t' || msg[nextCharacter]==='\n')){
+                        EXC+=`<span class='fuckat'>${msg.substr(i,Server.usrList[j].length+1)}</span>`;
+                        r=i+Server.usrList[j].length;
+                        break;
+                    }
+                    ifMatched=false;
+                }
+        }
+        if(ifMatched===false)
+            EXC+=msg[i];
+    }
+    msg=EXC;
+    // console.log(EXC);
     if(style){
         let StyleText=[];
         Object.keys(style).forEach(key=>{
--- Server.js
+++ Server.js
@@ -20,22 +20,129 @@ fs.writeFile(Filename,``,(err,stats)=>{
         process.exit();
     }
 })
+//Environment Vars
+const HACK_MSG_MX=10;
+//---Commandline Depot
+const readline=require('readline');
+var scanf=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+(()=>{
+    let LocalCommands={
+        "list":{
+            fun:()=>{
+                console.log(ServerInfo.usrList.map(usr=>'|---'+usr).join('\n'));
+            },
+            Parameter:false
+        },
+        "log":{
+            fun:()=>{
+                //当然，一开始就没怎么想着要做服务器日志的功能，所以显然只能输出一些没啥用的东西。
+                console.log(Bufp);
+            },
+            Parameter:false
+        },
+        "ban":{
+            fun:(usrn)=>{
+                let Bantotal=0;
+                Server.clients.forEach(Cli=>{
+                    if(usrn.indexOf(Cli.ClientId)!==-1){
+                        Cli.send(JSON.stringify({
+                            headers:{
+                                Content_Type:'application/message',
+                                Style:{"color":"#e7483f"}
+                            },
+                            body:"<i class='fas fa-ban' style='width:20px'></i> You are banned from the server."
+                        }));
+                        Cli.close();
+                        broadcast(`<i class="fa fa-exclamation-triangle" style="width:20px"></i> @${Cli.ClientId} is banned from the server.`,{"color":"#ffff00"})
+                        Bantotal++;
+                    }
+                });
+                console.log(`Banned ${Bantotal} users in total.`);
+            },
+            Parameter:true
+        }
+    };
+    let ThrowERR=[`欸，你刚才是不是输入了什么见不得人的东西啊~`,`不懂哦，唉。`,`根本搞不懂你在说什么！`,`不要乱来！`,`这样下去可是要被绑在耻辱柱上的哦~`];
+    CMD();
+    function CMD(){
+        scanf.question('CRL />',(input)=>{
+            (async ()=>{
+                input=input.trim().split(/\s+/);
+                if(LocalCommands[input[0]]){
+                    if(LocalCommands[input[0]].Parameter===true)
+                        await LocalCommands[input[0]].fun(input.splice(1,input.length-1));
+                    else await LocalCommands[input[0]].fun();
+                }else if(input[0]!=='')
+                    console.log(ThrowERR[Math.floor(Math.random()*ThrowERR.length)]);
+                CMD();
+            })();
+        });
+    }
+})();
 Server.on('connection',(ws,Req)=>{
     let ip=Req.connection.remoteAddress;
     let port=Req.connection.remotePort;
     let ClientId,Status=false;
+    let HACK_MSG_C=0;
+	let USER_NAME_WRONG=false;
+    // console.log(ws.url);
+    // ws.
     ws.on('message',msg=>{
-        msg=JSON.parse(msg);
+        try{
+            msg=JSON.parse(msg);
+        }catch(OOPS_LOOKS_LIKE_A_HACK_MESSAGE){
+            ws.send(msg);
+            HACK_MSG_C++;
+            if(HACK_MSG_C>=HACK_MSG_MX){
+                ws.send(JSON.stringify({
+                    headers:{
+                        Content_Type:'application/message',
+                        Style:{"color":"#e7483f"}
+                    },
+                    body:"<i class='fas fa-ban'></i> You are banned from the server."
+                }));
+                ws.close();
+                broadcast(`<i class="fa fa-exclamation-triangle"></i> @${ws.ClientId} is banned from the server for hack tries.`,{"color":"#ffff00"})
+            }
+            return;
+        }
+        if(msg.headers['Content_Type']==='application/userlist'){
+            // console.log(`${ClientId}`);
+            ServerInfo.time=new Date();
+            Status=true;
+            ws.send(JSON.stringify({
+                headers:{
+                    Content_Type:'application/userlist',
+                    Set_serverinfo:ServerInfo
+                }
+            }));
+        }
         if(msg.headers['Content_Type']==='application/init'){
             ClientId=msg.headers['Set_Name'];
             // console.log(`${ClientId}`);
+			for(var p=0;p<ServerInfo.usrList.length;p++)
+				if(ClientId===ServerInfo.usrList[p]){
+					ws.send(JSON.stringify({
+                        headers:{
+                            Content_Type:'application/message',
+                            Style:{"color":"#e7483f"}
+                        },
+                        body:"<i class='fas fa-info-circle' style='width:20px'></i> Error : Same Username Found."
+                    }));
+					USER_NAME_WRONG=true;
+					ws.close();return;
+				}
             ServerInfo.time=new Date();
-            let EventMsg=`[NOTE] ${ServerInfo.time.toTimeString().substring(0,8)}\n${ClientId} entered the chat room!`;
+            let EventMsg=`<i class="fas fa-user-plus" style="width:20px"></i> ${ServerInfo.time.toTimeString().substring(0,8)}\n${ClientId} entered the chat room!`;
             Bufp+=EventMsg;
             broadcast(EventMsg,{'color':'#13c60d'});
             broadcommand('UsrAdd',[ClientId]);
             ServerInfo.usrList.push(ClientId);
             Status=true;
+            ws.ClientId=ClientId;
             ws.send(JSON.stringify({
                 headers:{
                     Content_Type:'application/init',
@@ -43,15 +150,16 @@ Server.on('connection',(ws,Req)=>{
                 }
             }));
         }else if(msg.headers.Content_Type==='application/message'){
-            broadcast(`[INFO] ${ClientId}: ${msg.body}`);
+            ServerInfo.time=new Date();
+            broadcast(`<i class="fas fa-clock" style="width:20px"></i> ${ServerInfo.time.toTimeString().substring(0,8)} ${ClientId}: ${msg.body}`);
         }
     });
     ws.on('close',()=>{
-        if(ClientId){
+        if(ClientId && !USER_NAME_WRONG){
             ServerInfo.time=new Date();
-            let EventMsg=`[NOTE] ${ServerInfo.time.toTimeString().substring(0,8)}\n${ClientId} left the chat room!`;
+            let EventMsg=`<i class="fas fa-user-times" style="width:20px"></i> ${ServerInfo.time.toTimeString().substring(0,8)}\n${ClientId} left the chat room!`;
             Bufp+=EventMsg;
-            broadcast(EventMsg,{'color':'#13c60d'});
+            broadcast(EventMsg,{'color':'#e7483f'});
             broadcommand('UsrDel',[ClientId]);
             ServerInfo.usrList.splice(ServerInfo.usrList.indexOf(ClientId),1);
             Status=false;
